a120dc4d05227e7e7f2f079b2ddf3d65cee0c62d
Raouf Hecham <hecham.abdelraouf@gmail.com>
2019-08-25 14:18:56 +0200

    Fixed home page broken links:

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogConfig, MatDialog } from '@angular/material';
+import { HelpDialogComponent } from '../dialogs';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
   }
 
+  openHelpDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    const dialogRef = this.dialog.open(HelpDialogComponent, dialogConfig);
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  // brokerURL: 'wss://eldr.herokuapp.com:443/ws',
-  // apiRoot: "https://eldr.herokuapp.com:443/api"
+  brokerURL: 'wss://eldr.herokuapp.com:443/ws',
+  apiRoot: "https://eldr.herokuapp.com:443/api"
 
-  brokerURL: 'wss:localhost:8080/ws',
-  apiRoot: "http://localhost:8080/api"
+  // brokerURL: 'wss:localhost:8080/ws',
+  // apiRoot: "http://localhost:8080/api"
 };
